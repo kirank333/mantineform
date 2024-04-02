018bec832fe31b8c4a71d3a0de9124adabf33a23
gunalCyberminds <gunal@cybermindworks.com>
2024-04-02 11:22:55 +0530

    updated

--- penta/src/Sucess.jsx
+++ penta/src/Sucess.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '@mantine/core/styles.css';
 import { Center } from '@mantine/core';
+import IndianRupeeAmount from './IndianRupeeAmount';
 function Success() {
   return (
    
-    <div>Success</div>
+    <div>Success
+    
+        <IndianRupeeAmount amount={10000}/>
+    </div>
     
 
   )
